fc84c654ec3ceb4b4a33eb6989826e9cd892b213
PoopNoodles <sara00lorenzo@gmail.com>
2023-09-19 13:48:22 +0200

    corrigiendo error de sintaxis

--- Ejercicios/Ejercicio2.js
+++ Ejercicios/Ejercicio2.js
@@ -2,5 +2,5 @@ var multiplicador = 4;
 
 for (var i=0; i<=10; i++){
     var multiplicacion = i*multiplicador;
-    console.log(i + " * " multiplicador+" = " multiplicacion);
+    console.log(i + " * "+ multiplicador +" = "+ multiplicacion);
 }
