2ce1606249f36f6d43f1e2dbf79c4544a3d94f60
PoopNoodles <sara00lorenzo@gmail.com>
2023-09-19 11:36:34 +0200

    first commit
    added my code and its functions descriptions in comments

---
+++ variables.js
@@ -0,0 +1,24 @@
+var x1 = 3; // var puede contener un integer
+
+var x2; // se puede crear una var sin asignarle un valor
+x2 = 4+1; // se le asigna el valor después llamando a la var por el nombre qu ele hemos dado.
+
+var x3 = "hola"; //var puede contener un string de texto.
+
+var x4 = 2+1; //var puede contener una operación.
+var x5 = "2"+1; //operar con un string y un integer añadirá el integer al final del string.
+var x6 = "2"+"1"; //mismo resultado si operamos con dos números considerados strings
+
+var x7 = true; //var puede contener un valor booleano.
+var x8 = 5>4; // resultado booleano true al comprobar que 5 es en efecto mayor que 4.
+
+console.log(x1);
+
+console.log("x1:", x1);
+console.log("x2:", x2);
+console.log("x3:", x3);
+console.log("x4:", x4);
+console.log("x5:", x5);
+console.log("x6:", x6);
+console.log("x7:", x7);
+console.log("x8:", x8);
